4ee46b76c45187fb1152f7f28aee305be5782f62
tojoos <joosx2000@gmail.com>
2024-05-10 20:10:03 +0200

    Initial commit

---
+++ src/test/java/tojoos/temporarygraphetl/TemporaryGraphEtlApplicationTests.java
@@ -0,0 +1,13 @@
+package tojoos.temporarygraphetl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemporaryGraphEtlApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
