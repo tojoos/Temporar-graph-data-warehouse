7bd3d0da8191270aa89884863a15a36fbd867798
tojoos <joosx2000@gmail.com>
2024-05-13 12:09:49 +0200

    Add logger, tidy code

--- src/main/java/tojoos/temporarygraphetl/UserController.java
+++ src/main/java/tojoos/temporarygraphetl/UserController.java
@@ -1,5 +1,7 @@
 package tojoos.temporarygraphetl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,9 @@ import java.util.concurrent.ExecutionException;
 
 @RestController
 public class UserController {
+
+  private final static Logger log = LoggerFactory.getLogger(UserController.class);
+
   UserRepository userRepository;
   UserService userService;
 
@@ -27,34 +32,46 @@ public class UserController {
 
   @GetMapping("/users")
   public ResponseEntity<Set<ParentUserDto>> findAll(@RequestParam(required = false) LocalDateTime timestamp) {
+    log.info("Received incoming request... GET '/users'");
+    Set<ParentUserDto> usersFound;
     if (timestamp != null) {
-        return new ResponseEntity<>(userService.findAll(timestamp), HttpStatus.OK);
+      usersFound = userService.findAll(timestamp);
+      log.info("Successfully finished processing of request GET '/users'");
+      return new ResponseEntity<>(usersFound, HttpStatus.OK);
     }
 
-    return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    log.info("Timestamp not specified, getting latest data.");
+
+    usersFound = userService.findAll();
+    log.info("Successfully finished processing of request GET '/users'");
+    return new ResponseEntity<>(usersFound, HttpStatus.OK);
   }
 
   @PutMapping("/update")
   public ResponseEntity<?> update(@RequestBody List<UserDto> userDtos) throws UserNotFoundException {
+    log.info("Received incoming request... PUT '/update': " + userDtos);
     Set<ParentUserDto> updatedUsers = new HashSet<>();
     for (UserDto userDto : userDtos) {
       if (userDto == null || userDto.id() == null) {
+        log.error("User or userId is not set, aborting further processing");
         return new ResponseEntity<>( "Some users could not be uploaded due to missing id field", HttpStatus.BAD_REQUEST);
       }
       updatedUsers.add(userService.update(userDto));
     }
 
-    return new ResponseEntity<>(updatedUsers, HttpStatus.CREATED);
+    log.info("Successfully finished processing of PUT '/update': " + userDtos);
+    return new ResponseEntity<>(updatedUsers, HttpStatus.OK);
   }
 
   @PutMapping("/follow")
   public ResponseEntity<?> follow(@RequestBody List<Pair<Long, Long>> usersPairs) throws ExecutionException, InterruptedException {
+    log.info("Received incoming request... PUT '/follow': " + usersPairs.toString());
     CompletableFuture<Long> successfulOperations = CompletableFuture.supplyAsync(() -> {
       long counter = 0L;
       for (Pair<Long, Long> usersPair : usersPairs) {
         Long first = usersPair.getFirst();
         Long second = usersPair.getSecond();
-        // Perform follow process of users
+        // Perform follow process for each id pair
         userService.followUserById(first, second);
         counter++;
       }
@@ -62,18 +79,20 @@ public class UserController {
     });
 
     boolean allOperationsSuccessful = usersPairs.size() == successfulOperations.get();
+    log.info("Finished processing of PUT '/follow': " + usersPairs);
     return allOperationsSuccessful ? new ResponseEntity<>("All operations successful.", HttpStatus.OK) :
         new ResponseEntity<>( "Some operations were not successful", HttpStatus.BAD_REQUEST);
   }
 
   @PutMapping("/unfollow")
   public ResponseEntity<?> unfollow(@RequestBody List<Pair<Long, Long>> usersPairs) throws ExecutionException, InterruptedException {
+    log.info("Received incoming request... PUT '/unfollow': " + usersPairs.toString());
     CompletableFuture<Long> successfulOperations = CompletableFuture.supplyAsync(() -> {
       long counter = 0L;
       for (Pair<Long, Long> usersPair : usersPairs) {
         Long first = usersPair.getFirst();
         Long second = usersPair.getSecond();
-        // Perform follow process of users
+        // Perform unfollow process for each id pair
         userService.unfollowUserById(first, second);
         counter++;
       }
@@ -81,6 +100,7 @@ public class UserController {
     });
 
     boolean allOperationsSuccessful = usersPairs.size() == successfulOperations.get();
+    log.info("Finished processing of PUT '/unfollow': " + usersPairs);
     return allOperationsSuccessful ? new ResponseEntity<>("All operations successful.", HttpStatus.OK) :
         new ResponseEntity<>( "Some operations were not successful", HttpStatus.BAD_REQUEST);
   }
